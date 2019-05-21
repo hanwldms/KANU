f59371ad73b11aafe724a737b4df3bfada94da6d
booldook <booldook@gmail.com>
2019-05-21 16:00:10 +0900

    booldook

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 // main slider
 $('.slide1, .slide2, .slide3').hide();
 
-
-
 var slides = $('ul li.slide'),
 i = 0;
 
 
 $(slides[0]).show();
-(function loop() {
-slides.delay(3000).fadeOut(1000).eq(++i%slides.length).fadeIn(1000, function() {
-
+function loop() {
+    slides.delay(3000).fadeOut(1000);
+    slides.eq(++i%slides.length).fadeIn(1000, function() {
+        loop();
+    });
+}
 loop();
-  
-});
-}()); 
+
+
+
 
 // nav background
 $(window).scroll(function(){
-        var wScroll = $(this).scrollTop();
-        if (wScroll >= $("#section02").offset().top - 100){
-            $("nav").css({"background-color":"rgba(0, 0, 0, 0.9)"});
-        } else {
-            //$("nav").slideUp();
-            $("nav").css("background-color", "transparent");
-        }
+    var wScroll = $(this).scrollTop();
+    if (wScroll >= $("#section02").offset().top - 100){
+        $("nav").css({"background-color":"rgba(0, 0, 0, 0.9)"});
+    } else {
+        //$("nav").slideUp();
+        $("nav").css("background-color", "transparent");
+    }
+});
+
+var offset = [];
+$(window).resize(function(){
+    $(".section").each(function(i){
+        offset[i] = $(this).offset().top;
     });
+    console.log(offset);
+}).trigger("resize");
+
+$(".gnb > li").click(function(){
+    var i = $(this).index();
+    $("html, body").stop().animate({"scrollTop":offset[i]+"px"}, 500);
+});
